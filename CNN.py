bba820ce8ac29a64b71f5ee527654681f6ff657e
druczak1999 <65281661+druczak1999@users.noreply.github.com>
2021-12-03 12:12:54 +0100

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -4,10 +4,9 @@ from keras.datasets import mnist
 import matplotlib.pyplot as plt
 import keras
 from sklearn.model_selection import train_test_split
-from keras.models import Sequential,Input,Model
+from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import BatchNormalization
+from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D
 from keras.layers.advanced_activations import LeakyReLU
 import tensorflow as tf
 from sklearn.metrics import classification_report
@@ -41,6 +40,56 @@ class CNN:
         fashion_model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=(28,28,1),padding='same'))
         fashion_model.add(LeakyReLU(alpha=0.1))
         fashion_model.add(MaxPooling2D((2, 2),padding='same'))
+        fashion_model.add(Flatten())
+        fashion_model.add(Dense(128, activation='relu'))
+        fashion_model.add(LeakyReLU(alpha=0.1))                  
+        fashion_model.add(Dense(num_classes, activation='softmax'))
+        fashion_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=tf.keras.optimizers.Adam(),metrics=['accuracy'])
+        fashion_model.summary()
+        fashion_train = fashion_model.fit(train_X, train_label, batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(valid_X, valid_label))
+        test_eval = fashion_model.evaluate(test_X, test_Y_one_hot, verbose=0)
+        print('Test loss:', test_eval[0])
+        print('Test accuracy:', test_eval[1])
+
+        self.predict_labels(fashion_model, test_X, test_Y) 
+        self.make_charts(fashion_train)
+
+    def cnn_with_pooling_3(self,train_X,valid_X,train_label,valid_label, test_Y_one_hot, test_X, test_Y):
+        batch_size = 64
+        epochs = 10
+        num_classes = 10
+        fashion_model = Sequential()
+        fashion_model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=(28,28,1),padding='same'))
+        fashion_model.add(LeakyReLU(alpha=0.1))
+        fashion_model.add(MaxPooling2D((2, 2),padding='same'))
+        fashion_model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=(28,28,1),padding='same'))
+        fashion_model.add(LeakyReLU(alpha=0.1))
+        fashion_model.add(MaxPooling2D((2, 2),padding='same'))
+        fashion_model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=(28,28,1),padding='same'))
+        fashion_model.add(LeakyReLU(alpha=0.1))
+        fashion_model.add(MaxPooling2D((2, 2),padding='same'))
+        fashion_model.add(Flatten())
+        fashion_model.add(Dense(128, activation='relu'))
+        fashion_model.add(LeakyReLU(alpha=0.1))                  
+        fashion_model.add(Dense(num_classes, activation='softmax'))
+        fashion_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=tf.keras.optimizers.Adam(),metrics=['accuracy'])
+        fashion_model.summary()
+        fashion_train = fashion_model.fit(train_X, train_label, batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(valid_X, valid_label))
+        test_eval = fashion_model.evaluate(test_X, test_Y_one_hot, verbose=0)
+        print('Test loss:', test_eval[0])
+        print('Test accuracy:', test_eval[1])
+
+        self.predict_labels(fashion_model, test_X, test_Y) 
+        self.make_charts(fashion_train)
+
+    def cnn_with_pooling_avg(self,train_X,valid_X,train_label,valid_label, test_Y_one_hot, test_X, test_Y):
+        batch_size = 64
+        epochs = 10
+        num_classes = 10
+        fashion_model = Sequential()
+        fashion_model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=(28,28,1),padding='same'))
+        fashion_model.add(LeakyReLU(alpha=0.1))
+        fashion_model.add(AveragePooling2D((2, 2),padding='same'))
         fashion_model.add(Flatten())
         fashion_model.add(Dense(128, activation='relu'))
         fashion_model.add(LeakyReLU(alpha=0.1))                  
@@ -99,14 +148,13 @@ class CNN:
         self.predict_labels(fashion_model, test_X, test_Y) 
         self.make_charts(fashion_train)
 
-    def cnn_2d(self,train_X,valid_X,train_label,valid_label, test_Y_one_hot, test_X, test_Y):
+    def cnn_normal(self,train_X,valid_X,train_label,valid_label, test_Y_one_hot, test_X, test_Y):
         batch_size = 64
         epochs = 10
         num_classes = 10
         fashion_model = Sequential()
-        fashion_model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=(28,28,1),padding='same'))
-        fashion_model.add(LeakyReLU(alpha=0.1))
-        fashion_model.add(Flatten())  
+        fashion_model.add(Flatten(input_shape=(28,28)))
+        fashion_model.add(Dense(128, activation='relu'))              
         fashion_model.add(Dense(num_classes, activation='softmax'))
         fashion_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=tf.keras.optimizers.Adam(),metrics=['accuracy'])
         fashion_model.summary()
@@ -115,7 +163,7 @@ class CNN:
         print('Test loss:', test_eval[0])
         print('Test accuracy:', test_eval[1])
 
-        predict_labels(fashion_model, test_X, test_Y) 
+        self.predict_labels(fashion_model, test_X, test_Y) 
         self.make_charts(fashion_train)
 
     def make_charts(self, fashion_train):
@@ -167,4 +215,3 @@ if __name__ == '__main__':
     train_X,valid_X,train_label,valid_label, test_Y_one_hot, test_X, test_Y = cnn.read_and_convert_mnist()
 
     cnn.cnn_with_pooling(train_X,valid_X,train_label,valid_label, test_Y_one_hot, test_X, test_Y)
-
